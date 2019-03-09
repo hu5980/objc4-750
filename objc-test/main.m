9904900fcee64c1abda19fbc7ec57c2f52721cc2
胡光耀 <hugy@500wan.com>
2019-03-09 15:48:24 +0800

    工程目录改变

--- objc-test/main.m
+++ objc-test/main.m
@@ -9,7 +9,7 @@
 #import <objc/runtime.h>
 #import <objc/message.h>
 #import <malloc/malloc.h>
-
+#import <pthread.h>
 
 @interface Person : NSObject {
     int _age;
@@ -42,26 +42,33 @@ struct NSObject_IMPL {
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+//        NSObject *obj = [[NSObject alloc] init];
+//        NSLog(@"% zd",class_getInstanceSize([NSObject class]));  //8
+//        NSLog(@"%zu",malloc_size((__bridge const void *)obj)); // 16
+//
+//        printf("%lu\n",sizeof(double));
+//
+//        //  printf("%lu\n",sizeof(struct Person_IMPL));
+//        Person *person = [[Person alloc]init];
+//        //sizeof 是一个关键字 ，编译的时候就知道了
+//
+//        printf("%lu\n",sizeof(person));
+//        // 计算对象最少需要分配的内存
+//        NSLog(@"% zd",class_getInstanceSize([Person class]));  //28
+//        // 计算对象实际分配的内存
+//        NSLog(@"% zd",malloc_size((__bridge const void *)person)); //32
+//
+//        NSLog(@"%p",object_getClass([person class])); // 元类对象
+//        NSLog(@"%p", objc_getClass("Person")); // 类对象
+//        NSLog(@"%p", object_getClass(person)); // 类对象
+        
         NSObject *obj = [[NSObject alloc] init];
-        NSLog(@"% zd",class_getInstanceSize([NSObject class]));  //8
-        NSLog(@"%zu",malloc_size((__bridge const void *)obj)); // 16
-        
-        printf("%lu\n",sizeof(double));
-        
-        //  printf("%lu\n",sizeof(struct Person_IMPL));
-        Person *person = [[Person alloc]init];
-        //sizeof 是一个关键字 ，编译的时候就知道了
-        
-        printf("%lu\n",sizeof(person));
-        // 计算对象最少需要分配的内存
-        NSLog(@"% zd",class_getInstanceSize([Person class]));  //28
-        // 计算对象实际分配的内存
-        NSLog(@"% zd",malloc_size((__bridge const void *)person)); //32
-        
-        NSLog(@"%p",object_getClass([person class])); // 元类对象
-        NSLog(@"%p", objc_getClass("Person")); // 类对象
-        NSLog(@"%p", object_getClass(person)); // 类对象
-        
+       
+        for (int i = 0 ; i < 255; i++) {
+           
+             [obj retain];
+        }
+         [obj retainCount];
         
     }
     return 0;
